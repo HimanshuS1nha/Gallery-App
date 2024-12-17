9ddea30bc03066b548ff0d0b4e4cf8d9cf74ae87
Himanshu Sinha <ashusinha1904@gmail.com>
2024-12-17 22:12:11 +0530

    Expo MediaLibrary added

--- app/(tabs)/photos.tsx
+++ app/(tabs)/photos.tsx
@@ -1,12 +1,22 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React from "react";
+import { useQuery } from "@tanstack/react-query";
+import * as MediaLibrary from "expo-media-library";
+import tw from "twrnc";
 
 const Photos = () => {
+  const { data, isLoading } = useQuery({
+    queryKey: ["get-photos"],
+    queryFn: async () => {
+      const albumAssets = await MediaLibrary.getAssetsAsync();
+      return albumAssets.assets;
+    },
+  });
   return (
-    <View>
+    <View style={tw`flex-1 bg-white`}>
       <Text>Photos</Text>
     </View>
-  )
-}
+  );
+};
 
-export default Photos
+export default Photos;
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,43 @@
-import { Text, View } from "react-native";
+import { router, useRootNavigationState } from "expo-router";
+import { useCallback, useEffect } from "react";
+import {
+  ActivityIndicator,
+  Alert,
+  BackHandler,
+  Image,
+  Linking,
+  View,
+} from "react-native";
+import tw from "twrnc";
+import * as MediaLibrary from "expo-media-library";
 
 export default function Index() {
+  const rootNavigationState = useRootNavigationState();
+
+  const [permissionResponse, requestPermission] = MediaLibrary.usePermissions();
+
+  const getPermission = useCallback(async () => {
+    if (permissionResponse?.status !== "granted") {
+      await requestPermission();
+    }
+  }, []);
+
+  useEffect(() => {
+    if (rootNavigationState?.key) {
+      if (permissionResponse) {
+        getPermission().then(() => {
+          router.replace("/photos");
+        });
+      }
+    }
+  }, [rootNavigationState?.key, permissionResponse?.status]);
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={tw`flex-1 justify-center items-center gap-y-5 bg-white`}>
+      <Image
+        source={require("../assets/images/logo.png")}
+        style={tw`w-54 h-54`}
+      />
+      <ActivityIndicator size={45} color={"blue"} />
     </View>
   );
 }
